f201b7076590b9ae469a0870df0cdc07017f62a5
adilsonv77@gmail.com <adilsonv77@gmail.com@1eb9bf7e-ced2-3440-c91b-7b24e603ad30>
2014-07-10 13:52:04 +0000

    The action cycle was rewritten. isThereACustomer implemented.

--- trunk/web/nobugs.js
+++ trunk/web/nobugs.js
@@ -40,7 +40,7 @@ document.write('<script type="text/javascript" src="generated/' +
 var Game = {};
 
 var hero = new SnackMan();
-Game.mission = 0;
+Game.mission = BlocklyApps.getStringParamFromUrl('mission', '1');
 
 
 /**
@@ -98,51 +98,138 @@ Game.init = function() {
   // Example how can assign a method to a button
   //  BlocklyApps.bindClick('captureButton', Game.createImageLink);
 
-  var defaultXml =
-	  /*
-      '<xml>' +
-      '  <block type="controls_for" x="70" y="70">'+
-      '    <value name="FROM">'+
-      '      <block type="math_number">' +
-      '        <field name="NUM">1</field>' +
-      '      </block>' +
-      '    </value>' +
-      '    <value name="TO">'+
-      '      <block type="math_number">' +
-      '        <field name="NUM">3</field>' +
-      '      </block>' +
-      '    </value>' +
-      '    <value name="BY">'+
-      '      <block type="math_number">' +
-      '        <field name="NUM">1</field>' +
-      '      </block>' +
-      '    </value>' +
-      '    <statement name="DO">' +
-		  '  <block type="move_goToCustomer">' +
-		  '    <value name="VALUE">' +
-		  '      <block type="variables_get">' +
-		  '        <field name="VAR">i</field>' +
-		  '      </block>' +
-		  '    </value>' +
-		  '    <next>'+
-			  '  <block type="move_goToDisplay">' +
-			  '  </block>' +
-		  '   </next>' +
+  var defaultXml = "";
+  if (Game.mission === '2') {
+	  defaultXml =
+  '<xml xmlns="http://www.w3.org/1999/xhtml">' +
+  '<block type="variables_set" id="43" inline="true" x="116" y="-17">' +
+   ' <field name="VAR">i</field>' +
+    '<value name="VALUE">' +
+     ' <block type="math_number" id="45">' +
+      '  <field name="NUM">1</field>' +
+      '</block>' +
+'    </value>' +
+ '   <next>' +
+  '    <block type="variables_set" id="6" inline="true">' +
+   '     <field name="VAR">sair</field>' +
+    '    <value name="VALUE">' +
+     '     <block type="logic_boolean" id="12">' +
+      '      <field name="BOOL">FALSE</field>' +
+       '   </block>' +
+        '</value>' +
+'        <next>' +
+ '         <block type="controls_whileUntil" id="18" inline="false">' +
+  '          <field name="MODE">WHILE</field>' +
+   '         <value name="BOOL">' +
+    '          <block type="logic_negate" id="33" inline="false">' +
+     '           <value name="BOOL">' +
+      '            <block type="variables_get" id="38">' +
+       '             <field name="VAR">sair</field>' +
+        '          </block>' +
+         '       </value>' +
+          '    </block>' +
+           ' </value>' +
+'            <statement name="DO">' +
+ '             <block type="move_goToCustomer" id="1" inline="false">' +
+  '              <value name="VALUE">' +
+   '               <block type="variables_get" id="52">' +
+    '                <field name="VAR">i</field>' +
+     '             </block>' +
+      '          </value>' +
+       '         <next>' +
+        '          <block type="controls_if" id="65" inline="false">' +
+         '           <mutation else="1"></mutation>' +
+          '          <value name="IF0">' +
+           '           <block type="ask_isThereACustomer" id="85"></block>' +
+            '        </value>' +
+             '       <statement name="DO0">' +
+              '        <block type="move_goToCooler" id="93">' +
+               '         <next>' +
+                '          <block type="variables_set" id="100" inline="true">' +
+                 '           <field name="VAR">sair</field>' +
+                  '          <value name="VALUE">' +
+                   '           <block type="logic_boolean" id="106">' +
+                    '            <field name="BOOL">TRUE</field>' +
+                     '         </block>' +
+                      '      </value>' +
+                       '   </block>' +
+                        '</next>' +
+'                      </block>' +
+ '                   </statement>' +
+  '                  <statement name="ELSE">' +
+   '                   <block type="controls_if" id="116" inline="false">' +
+    '                    <mutation else="1"></mutation>' +
+     '                   <value name="IF0">' +
+      '                    <block type="logic_compare" id="128" inline="true">' +
+       '                     <field name="OP">EQ</field>' +
+        '                    <value name="A">' +
+         '                     <block type="variables_get" id="135">' +
+          '                      <field name="VAR">i</field>' +
+           '                   </block>' +
+           '                 </value>' +
+            '                <value name="B">' +
+             '                 <block type="math_number" id="137">' +
+              '                  <field name="NUM">1</field>' +
+               '               </block>' +
+                '            </value>' +
+                 '         </block>' +
+                  '      </value>' +
+                   '     <statement name="DO0">' +
+                    '      <block type="variables_set" id="144" inline="true">' +
+                     '       <field name="VAR">i</field>' +
+                      '      <value name="VALUE">' +
+                       '       <block type="math_number" id="145">' +
+                        '        <field name="NUM">2</field>' +
+                         '     </block>' +
+                          '  </value>' +
+'                          </block>' +
+ '                       </statement>' +
+  '                      <statement name="ELSE">' +
+   '                       <block type="variables_set" id="150" inline="true">' +
+    '                        <field name="VAR">i</field>' +
+     '                       <value name="VALUE">' +
+      '                        <block type="math_number" id="151">' +
+       '                         <field name="NUM">1</field>' +
+        '                      </block>' +
+         '                   </value>' +
+          '                </block>' +
+           '             </statement>' +
+            '          </block>' +
+             '       </statement>' +
+              '    </block>' +
+               ' </next>' +
+'              </block>' +
+ '           </statement>' +
+  '        </block>' +
+   '     </next>' +
+    '  </block>' +
+'    </next>' +
+'  </block>' +
+'</xml>';
+  } else {
+	  defaultXml =
+	  '<xml>' +
+	  '  <block type="move_goToCustomer">' +
+	  '    <value name="VALUE">' +
+	  '      <block type="math_number">' +
+	  '        <field name="NUM">1</field>' +
+	  '      </block>' +
+	  '    </value>' +
+	  '    <next>'+
+		  '  <block type="move_goToDisplay">' +
 		  '  </block>' +
+	  '   </next>' +
+	  '  </block>' +
+	  '</xml>';
+  }
+  
 
-      '    </statement>' + 
-      '  </block>' +
-      '</xml>';
-      */
-  '<xml>' +
-  '  <block type="move_goToDisplay">' +
-  '  </block>' +
-  '</xml>';
 
   BlocklyApps.loadBlocks(defaultXml);
 
   BlocklyApps.bindClick('runButton', Game.runButtonClick);
   BlocklyApps.bindClick('resetButton', Game.resetButtonClick);
+  //BlocklyApps.bindClick('xmlButton', Game.xmlButtonClick);
 
   Game.ctxDisplay = document.getElementById('display').getContext('2d');
   
@@ -220,11 +307,19 @@ Game.reset = function() {
  *     the states.
  */
 Game.display = function() {
-	
+
+	CustomerManager.animation();
+
 	Game.ctxDisplay.drawImage( Game.imgBackground, 0 , 0, 352, 448 );
 	hero.draw(Game.ctxDisplay);
 	CustomerManager.draw(Game.ctxDisplay);
 	
+};
+
+Game.xmlButtonClick = function() {
+	//var code = Blockly.Xml.domToPrettyText(Blockly.Xml.workspaceToDom(Blockly.mainWorkspace));
+	var code = Blockly.JavaScript.workspaceToCode();
+	alert(code);
 };
 
 /**
@@ -263,24 +358,26 @@ Game.resetButtons = function() {
  */
 Game.execute = function() {
   BlocklyApps.log = [];
-  BlocklyApps.ticks = 1000000;
+  BlocklyApps.ticks = 10000; // how many loops are acceptable before the system define it is in infinite loop ? 
+
+  // Reset the graphic.
+  Game.reset();
 
   var code = Blockly.JavaScript.workspaceToCode();
   try {
     eval(code);
   } catch (e) {
-    // Null is thrown for infinite loop.
-    // Otherwise, abnormal termination is a user error.
-    if (e !== Infinity) {
-      alert(e);
-      Game.resetButtons();
-      return;
-    }
+	  
+	  if (e == Infinity) { 
+		  Game.showError("Error_infinityLoopDetected");
+	      Game.resetButtons();
+	      return;
+	  }
+	  
   }
 
   // BlocklyApps.log now contains a transcript of all the user's actions.
-  // Reset the graphic and animate the transcript.
-  Game.reset();
+  Game.stepSpeed = 1000 * Math.pow(0.5, 3);
   Game.pidList.push( window.setTimeout(function() {Game.animate();}, 100) );
 };
 
@@ -298,16 +395,15 @@ Game.animate = function() {
   var command = tuple.shift();
   BlocklyApps.highlight(tuple.pop());
   
-  Game.stepSpeed = 1000 * Math.pow(0.5, 3);
-  
-  var t = Game.step(command, tuple) + 1;
-  if (t == 0) { // something wrong happened
-	 // all processing of the wrong was scheduled
-	  return; 
-  }
+  if (Game.step(command, tuple))
+	  // call the next animate when the animation of the last command has finished
+	  Game.pidList.push( window.setTimeout(function() {Game.animate();}, Game.stepSpeed) );
+  else {
 
-  // call the next animate when the animation of the last command has finished
-  Game.pidList.push( window.setTimeout(function() {Game.animate();}, Game.stepSpeed*t) );
+	  Game.resetButtons();
+	  Blockly.mainWorkspace.highlightBlock(null);
+	  
+  }
 };
 
 /**
@@ -317,31 +413,36 @@ Game.animate = function() {
  */
 Game.step = function(command, values) {
   switch (command) {
-  case 'GTC' :
-	  return hero.animateGoToConsumer(values);
-  case 'GTD':
-	  return hero.animateGoToDisplay();
-  case 'GTCO':
-	  return hero.animateGoToCooler();
+    case 'AL' :
+    	hero.alertRun(values);
+    	break;
+    	
+  	case 'IM' :
+  		hero.changeSnackManImage(values);
+  		break;
+  
+  	case 'MS' :
+  		hero.changeSnackManPosition(values.shift(), values.shift(), values.shift(), values.shift());
+  		break;
+  		
+  	case 'fail':
+  		Game.showError(values);
+  		return false;
   }
+  
+  return true;
 };
 
-Game.scheduleError = function(iderror) {
+Game.showError = function(iderror) {
 	
 	var content = document.getElementById('dialogError');
 	var container = document.getElementById('dialogErrorText');
 	container.textContent = BlocklyApps.getMsg(iderror);
 	
-    var style = {width: '370px', top: '120px'};
+    var style = {top: '120px'}; // };//{width: '370px', 
 	style[Blockly.RTL ? 'right' : 'left'] = '215px';
 	var origin = Blockly.mainWorkspace.topBlocks_[Blockly.mainWorkspace.topBlocks_.length-1].getSvgRoot();
 	
-	Game.pidList.push( window.setTimeout(
-			function(content, origin, style) {
-				BlocklyApps.showDialog(content, origin, true, true, style, null);
-			}.bind(this, content, origin, style), Game.stepSpeed) );
-	
-	
-   
-	
+	BlocklyApps.showDialog(content, origin, true, true, style, null);
+
 };
--- trunk/web/nobugs/customerman.js
+++ trunk/web/nobugs/customerman.js
@@ -12,15 +12,42 @@ CustomerManager.reset = function() {
 CustomerManager.update = function() {
 	if (customers.length == 0)
 		customers[0] = new Customer({
-			place: "counte1",
+			place: CustOpt.counter1,
 			id: "01"
 		});
 	
-	customers[0].update();
+	for (var i = 0; i < customers.length; i++)
+		customers[i].update();
+	
+};
+
+CustomerManager.animation = function() {
+	
+	for (var i = 0; i < customers.length; i++)
+		customers[i].animate();
 	
 };
 
 CustomerManager.draw = function(ctx) {
 	for (var i=0; i<customers.length; i++)
 		customers[i].draw(ctx);
+};
+
+CustomerManager.isThereACustomerCounter1 = function() {
+	
+	for (var i=0; i<customers.length; i++)
+		if (customers[i].currentNode.id === CustOpt.counter1)
+			return true;
+	
+	return false;
+};
+
+CustomerManager.isThereACustomerCounter2 = function() {
+	
+	for (var i=0; i<customers.length; i++)
+		if (customers[i].currentNode.id === CustOpt.counter2)
+			return true;
+	
+	return false;
+	
 };
