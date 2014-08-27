de65d4eb52430685b982201122d2c7a6c0d9448b
adilsonv77@gmail.com <adilsonv77@gmail.com@1eb9bf7e-ced2-3440-c91b-7b24e603ad30>
2014-08-27 12:50:31 +0000

    - load missions by XML file - the four customer´s position close to the counter

--- web/nobugs/customer.js
+++ web/nobugs/customer.js
@@ -2,7 +2,7 @@
  * NoBug's Snack Bar
  *
  * Copyright 2014 Adilson Vahldick.
- * https://meteoricsnackbar.googlecode.com/
+ * https://nobugssnackbar.googlecode.com/
  * 
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -25,11 +25,12 @@
 'use strict';
 
 var CustOpt = {};
-CustOpt.path = {n2:{n1:1, n3:1}, n3:{n2:1, n4:1}, n4:{n3:1, n5:1}, n5:{n4:1, n6:1}, n6:{n5:1, n7:1}, n7:{n6:1, n8:1}, n8:{n7:1, n9:1}, n9:{n8:1, n10:1}, n10:{n9:1, n11:1}, n11:{n10:1, n12:1}, n12:{n11:1, n13:1}, n13:{n12:1, n14:1}, n14:{n13:1, n15:1}, n15:{n14:1, n16:1}, n16:{n15:1, n17:1}, n17:{n16:1, n18:1}, n18:{n17:1, n19:1}, n19:{n18:1, n20:1}, n20:{n19:1, n21:1}, n21:{n20:1, n22:1}, n22:{n21:1, n23:1}, n23:{n22:1, n24:1}, n24:{n23:1, n25:1}, n25:{n24:1, n26:1}, n26:{n25:1, n27:1}, n27:{n26:1, n28:1}, n28:{n27:1, n29:1}, n29:{n28:1, n30:1}, n30:{n29:1, n31:1}, n31:{n30:1, n32:1}, n32:{n31:1, n33:1}, n33:{n32:1, n34:1}, n34:{n33:1, n35:1}, n35:{n34:1, n36:1}, n36:{n35:1, n37:1}, n37:{n36:1, n38:1}, n38:{n37:1, n39:1}, n39:{n38:1, n40:1}, n40:{n39:1, n41:1}, n41:{n40:1, n42:1}, n42:{n41:1, n43:1}, n1:{n2:1}, n43:{n42:1} };
-CustOpt.nodes = {n1:{id: 'n1', x:14, y: 96}, n2:{id: 'n2', x:14, y: 102}, n3:{id: 'n3', x:14, y: 108}, n4:{id: 'n4', x:14, y: 114}, n5:{id: 'n5', x:14, y: 120}, n6:{id: 'n6', x:14, y: 126}, n7:{id: 'n7', x:14, y: 132}, n8:{id: 'n8', x:14, y: 138}, n9:{id: 'n9', x:14, y: 144}, n10:{id: 'n10', x:14, y: 150}, n11:{id: 'n11', x:14, y: 156}, n12:{id: 'n12', x:14, y: 162}, n13:{id: 'n13', x:14, y: 168}, n14:{id: 'n14', x:14, y: 174}, n15:{id: 'n15', x:14, y: 180}, n16:{id: 'n16', x:14, y: 186}, n17:{id: 'n17', x:14, y: 192}, n18:{id: 'n18', x:14, y: 198}, n19:{id: 'n19', x:14, y: 204}, n20:{id: 'n20', x:14, y: 210}, n21:{id: 'n21', x:14, y: 216}, n22:{id: 'n22', x:14, y: 222}, n23:{id: 'n23', x:14, y: 228}, n24:{id: 'n24', x:14, y: 234}, n25:{id: 'n25', x:14, y: 240}, n26:{id: 'n26', x:14, y: 246}, n27:{id: 'n27', x:14, y: 252}, n28:{id: 'n28', x:14, y: 258}, n29:{id: 'n29', x:14, y: 264}, n30:{id: 'n30', x:14, y: 270}, n31:{id: 'n31', x:14, y: 276}, n32:{id: 'n32', x:14, y: 282}, n33:{id: 'n33', x:14, y: 288}, n34:{id: 'n34', x:17, y: 290}, n35:{id: 'n35', x:20, y: 290}, n36:{id: 'n36', x:23, y: 290}, n37:{id: 'n37', x:26, y: 290}, n38:{id: 'n38', x:29, y: 290}, n39:{id: 'n39', x:32, y: 290}, n40:{id: 'n40', x:35, y: 290}, n41:{id: 'n41', x:38, y: 290}, n42:{id: 'n42', x:41, y: 290}, n43:{id: 'n43', x:44, y: 290} };
-CustOpt.keynodes = ['n1', 'n43' ];
+CustOpt.path = {n2:{n1:1, n3:1}, n3:{n2:1, n4:1}, n4:{n3:1, n5:1}, n5:{n4:1, n6:1}, n6:{n5:1, n7:1}, n7:{n6:1, n8:1}, n8:{n7:1, n9:1}, n9:{n8:1, n10:1}, n10:{n9:1, n11:1}, n11:{n10:1, n12:1}, n12:{n11:1, n13:1}, n13:{n12:1, n14:1}, n14:{n13:1, n15:1}, n15:{n14:1, n16:1}, n16:{n15:1, n17:1}, n17:{n16:1, n18:1}, n18:{n17:1, n19:1}, n19:{n18:1, n20:1}, n20:{n19:1, n21:1}, n21:{n20:1, n22:1}, n22:{n21:1, n23:1}, n23:{n22:1, n24:1}, n24:{n23:1, n25:1}, n25:{n24:1, n26:1}, n26:{n25:1, n27:1}, n27:{n26:1, n28:1}, n28:{n27:1, n29:1}, n29:{n28:1, n30:1}, n30:{n29:1, n31:1}, n31:{n30:1, n32:1}, n32:{n31:1, n33:1}, n34:{n33:1, n35:1}, n35:{n34:1, n36:1}, n36:{n35:1, n37:1}, n37:{n36:1, n38:1}, n38:{n37:1, n39:1}, n39:{n38:1, n40:1}, n40:{n39:1, n41:1}, n41:{n40:1, n42:1}, n42:{n41:1, n43:1}, n33:{n32:1, n34:1, n44:1}, n44:{n33:1, n45:1}, n45:{n44:1, n46:1}, n46:{n45:1, n47:1}, n47:{n46:1, n48:1}, n48:{n47:1, n49:1}, n49:{n48:1, n50:1}, n50:{n49:1, n51:1}, n51:{n50:1, n52:1}, n52:{n51:1, n53:1}, n54:{n53:1, n55:1}, n55:{n54:1, n56:1}, n56:{n55:1, n57:1}, n57:{n56:1, n58:1}, n58:{n57:1, n59:1}, n59:{n58:1, n60:1}, n60:{n59:1, n61:1}, n61:{n60:1, n62:1}, n62:{n61:1, n63:1}, n53:{n52:1, n54:1, n64:1}, n64:{n53:1, n65:1}, n65:{n64:1, n66:1}, n66:{n65:1, n67:1}, n67:{n66:1, n68:1}, n68:{n67:1, n69:1}, n69:{n68:1, n70:1}, n70:{n69:1, n71:1}, n71:{n70:1, n72:1}, n72:{n71:1, n73:1}, n74:{n73:1, n75:1}, n75:{n74:1, n76:1}, n76:{n75:1, n77:1}, n77:{n76:1, n78:1}, n78:{n77:1, n79:1}, n79:{n78:1, n80:1}, n80:{n79:1, n81:1}, n81:{n80:1, n82:1}, n82:{n81:1, n83:1}, n73:{n72:1, n74:1, n84:1}, n84:{n73:1, n85:1}, n85:{n84:1, n86:1}, n86:{n85:1, n87:1}, n87:{n86:1, n88:1}, n88:{n87:1, n89:1}, n89:{n88:1, n90:1}, n90:{n89:1, n91:1}, n91:{n90:1, n92:1}, n92:{n91:1, n93:1}, n94:{n93:1, n95:1}, n95:{n94:1, n96:1}, n96:{n95:1, n97:1}, n97:{n96:1, n98:1}, n98:{n97:1, n99:1}, n99:{n98:1, n100:1}, n100:{n99:1, n101:1}, n101:{n100:1, n102:1}, n102:{n101:1, n103:1}, n93:{n92:1, n94:1}, n1:{n2:1}, n43:{n42:1}, n63:{n62:1}, n83:{n82:1}, n103:{n102:1} };
+CustOpt.nodes = {n1:{id: 'n1', x:14, y: 96}, n2:{id: 'n2', x:14, y: 102}, n3:{id: 'n3', x:14, y: 108}, n4:{id: 'n4', x:14, y: 114}, n5:{id: 'n5', x:14, y: 120}, n6:{id: 'n6', x:14, y: 126}, n7:{id: 'n7', x:14, y: 132}, n8:{id: 'n8', x:14, y: 138}, n9:{id: 'n9', x:14, y: 144}, n10:{id: 'n10', x:14, y: 150}, n11:{id: 'n11', x:14, y: 156}, n12:{id: 'n12', x:14, y: 162}, n13:{id: 'n13', x:14, y: 168}, n14:{id: 'n14', x:14, y: 174}, n15:{id: 'n15', x:14, y: 180}, n16:{id: 'n16', x:14, y: 186}, n17:{id: 'n17', x:14, y: 192}, n18:{id: 'n18', x:14, y: 198}, n19:{id: 'n19', x:14, y: 204}, n20:{id: 'n20', x:14, y: 210}, n21:{id: 'n21', x:14, y: 216}, n22:{id: 'n22', x:14, y: 222}, n23:{id: 'n23', x:14, y: 228}, n24:{id: 'n24', x:14, y: 234}, n25:{id: 'n25', x:14, y: 240}, n26:{id: 'n26', x:14, y: 246}, n27:{id: 'n27', x:14, y: 252}, n28:{id: 'n28', x:14, y: 258}, n29:{id: 'n29', x:14, y: 264}, n30:{id: 'n30', x:14, y: 270}, n31:{id: 'n31', x:14, y: 276}, n32:{id: 'n32', x:14, y: 282}, n33:{id: 'n33', x:14, y: 288}, n34:{id: 'n34', x:17, y: 290}, n35:{id: 'n35', x:20, y: 290}, n36:{id: 'n36', x:23, y: 290}, n37:{id: 'n37', x:26, y: 290}, n38:{id: 'n38', x:29, y: 290}, n39:{id: 'n39', x:32, y: 290}, n40:{id: 'n40', x:35, y: 290}, n41:{id: 'n41', x:38, y: 290}, n42:{id: 'n42', x:41, y: 290}, n43:{id: 'n43', x:44, y: 290}, n44:{id: 'n44', x:14, y: 294}, n45:{id: 'n45', x:14, y: 298}, n46:{id: 'n46', x:14, y: 302}, n47:{id: 'n47', x:14, y: 306}, n48:{id: 'n48', x:14, y: 310}, n49:{id: 'n49', x:14, y: 314}, n50:{id: 'n50', x:14, y: 318}, n51:{id: 'n51', x:14, y: 322}, n52:{id: 'n52', x:14, y: 326}, n53:{id: 'n53', x:14, y: 330}, n54:{id: 'n54', x:17, y: 330}, n55:{id: 'n55', x:20, y: 330}, n56:{id: 'n56', x:23, y: 330}, n57:{id: 'n57', x:26, y: 330}, n58:{id: 'n58', x:29, y: 330}, n59:{id: 'n59', x:32, y: 330}, n60:{id: 'n60', x:35, y: 330}, n61:{id: 'n61', x:38, y: 330}, n62:{id: 'n62', x:41, y: 330}, n63:{id: 'n63', x:44, y: 330}, n64:{id: 'n64', x:14, y: 334}, n65:{id: 'n65', x:14, y: 338}, n66:{id: 'n66', x:14, y: 342}, n67:{id: 'n67', x:14, y: 346}, n68:{id: 'n68', x:14, y: 350}, n69:{id: 'n69', x:14, y: 354}, n70:{id: 'n70', x:14, y: 358}, n71:{id: 'n71', x:14, y: 362}, n72:{id: 'n72', x:14, y: 366}, n73:{id: 'n73', x:14, y: 370}, n74:{id: 'n74', x:17, y: 370}, n75:{id: 'n75', x:20, y: 370}, n76:{id: 'n76', x:23, y: 370}, n77:{id: 'n77', x:26, y: 370}, n78:{id: 'n78', x:29, y: 370}, n79:{id: 'n79', x:32, y: 370}, n80:{id: 'n80', x:35, y: 370}, n81:{id: 'n81', x:38, y: 370}, n82:{id: 'n82', x:41, y: 370}, n83:{id: 'n83', x:44, y: 370}, n84:{id: 'n84', x:14, y: 374}, n85:{id: 'n85', x:14, y: 378}, n86:{id: 'n86', x:14, y: 382}, n87:{id: 'n87', x:14, y: 386}, n88:{id: 'n88', x:14, y: 390}, n89:{id: 'n89', x:14, y: 394}, n90:{id: 'n90', x:14, y: 398}, n91:{id: 'n91', x:14, y: 402}, n92:{id: 'n92', x:14, y: 406}, n93:{id: 'n93', x:14, y: 410}, n94:{id: 'n94', x:17, y: 410}, n95:{id: 'n95', x:20, y: 410}, n96:{id: 'n96', x:23, y: 410}, n97:{id: 'n97', x:26, y: 410}, n98:{id: 'n98', x:29, y: 410}, n99:{id: 'n99', x:32, y: 410}, n100:{id: 'n100', x:35, y: 410}, n101:{id: 'n101', x:38, y: 410}, n102:{id: 'n102', x:41, y: 410}, n103:{id: 'n103', x:44, y: 410} };
+CustOpt.keynodes = ['n1', 'n43', 'n63', 'n83', 'n103' ];
 
-CustOpt.counter = ['n43', 'xxx', 'yyy', 'zzz'];
+CustOpt.counter = ['n43', 'n63', 'n83', 'n103'];
+CustOpt.door = 'n1';
 
 CustOpt.customerFinalPath = new Array();
 
@@ -47,24 +48,24 @@ CustOpt.createCustomerPath();
 var Customer = {};
 
 Customer = function(options) {
-	this.currentNode = CustOpt.customerFinalPath[0];
+	
+	this.currentNode = CustOpt.nodes[options.init];
 	this.dest = CustOpt.nodes[options.place];
 	
 	this.place = options.place;
-	this.showCustomer = false;
 	this.showDoor = true;
-	this.state = 0;
+	// if he is in the door, then he is in state = 0 else state = 7
+	this.state = (this.currentNode.id === CustOpt.customerFinalPath[0].id?0:7);
+	this.showCustomer = this.state !== 0;
 	
 	this.log = [];
-	this.x = 0;
-	this.y = 0;
 	
 	this.img = new Sprite({
 		ticksPerFrame: 0,
 		numberOfFrames: 3,
 		horzSeq: true,
-		x: 14,
-		y: 64,
+		x: this.currentNode.x,
+		y: this.currentNode.y - 32,
 		width: 96,
 		height: 32,
 		sourceY: 0,
@@ -81,6 +82,11 @@ Customer = function(options) {
 		height: 256,
 		imgSrc : "images/doors.png"
 	});
+	
+	this.coin =new Image();
+	this.coin.src = "images/coin.png";
+	
+	this.showCoin = false;
 
 };
 
@@ -117,8 +123,12 @@ Customer.prototype.update = function() {
 			}
 			break;
 			
+	// turn to front
+	case 7: this.log.push(['MC', this.currentNode.x, this.currentNode.y, this.currentNode.x, this.currentNode.y]);
+			break;
+			
 	// finish state: nothing to do on this moment
-	case 7: return;
+	case 8: return;
 	}
 
 	this.state++;
@@ -176,8 +186,30 @@ Customer.prototype.draw = function(ctx) {
 		this.img.draw(ctx);
 	}
 		
+	if (this.showCoin) {
+		ctx.drawImage(this.coin, this.img.x, this.img.y-8, 8, 8);
+	}
+		
 };
 
 Customer.prototype.askForDrink = function() {
 	return {qt:1, type: "drink", descr:"$$coke"};
+};
+
+Customer.prototype.deliver = function(item) {
+	
+	var happy = false;
+	
+	if (item.type === "drink") {
+	
+		happy = (item.descr === "$$coke") && (item.qt === 1);
+		
+	}
+	
+	if (happy) {
+		this.showCoin = true;
+	}
+	
+	this.draw();
+	
 };
