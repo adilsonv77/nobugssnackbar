e2edde849e3eed4001d4ca2d79b880584ff424ae
adilsonv77@gmail.com <adilsonv77@gmail.com@1eb9bf7e-ced2-3440-c91b-7b24e603ad30>
2014-10-28 11:24:44 +0000

    Development of Issue #41

---
+++ trunk/src/pt/uc/dei/nobugssnackbar/model/Question.java
@@ -0,0 +1,59 @@
+package pt.uc.dei.nobugssnackbar.model;
+
+import java.util.List;
+
+import org.directwebremoting.annotations.DataTransferObject;
+import org.directwebremoting.annotations.RemoteProperty;
+
+@DataTransferObject()
+public class Question {
+
+	@RemoteProperty
+	private long id;
+	
+	@RemoteProperty
+	private String description;
+	
+	@RemoteProperty
+	private boolean required;
+	
+	@RemoteProperty
+	private String type;
+	
+	@RemoteProperty
+	private List<String> options;
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	public boolean isRequired() {
+		return required;
+	}
+	public void setRequired(boolean required) {
+		this.required = required;
+	}
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	public List<String> getOptions() {
+		return options;
+	}
+	public void setOptions(List<String> options) {
+		this.options = options;
+	}
+	
+	
+	
+}
---
+++ trunk/src/pt/uc/dei/nobugssnackbar/model/Questionnaire.java
@@ -0,0 +1,45 @@
+package pt.uc.dei.nobugssnackbar.model;
+
+import java.util.List;
+
+import org.directwebremoting.annotations.DataTransferObject;
+import org.directwebremoting.annotations.RemoteProperty;
+
+@DataTransferObject()
+public class Questionnaire {
+
+	private long id;
+	
+	@RemoteProperty
+	private String description;
+	
+	@RemoteProperty
+	private List<Question> questions;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public List<Question> getQuestions() {
+		return questions;
+	}
+
+	public void setQuestions(List<Question> questions) {
+		this.questions = questions;
+	}
+	
+	
+	
+}
