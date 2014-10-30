6d2640886437201d8a2549ccb905807cee02661d
adilsonv77@gmail.com <adilsonv77@gmail.com@1eb9bf7e-ced2-3440-c91b-7b24e603ad30>
2014-10-30 15:53:12 +0000

    Disable the debug button while the animation is running

---
+++ trunk/utils/nobugssnackbar/GenerateUsers.java
@@ -0,0 +1,39 @@
+package nobugssnackbar;
+
+import java.util.Random;
+
+import pt.uc.dei.nobugssnackbar.control.UserControl;
+import pt.uc.dei.nobugssnackbar.dao.NoBugsConnection;
+
+public class GenerateUsers {
+
+	private static Random r;
+	private static String s;
+
+	private static String extractChar() {
+		
+		int x = r.nextInt(s.length()-1) + 1;
+		return s.substring(x, x+1);
+	}
+	
+	public static void main(String[] args) throws Exception {
+		
+		NoBugsConnection.buildConnection("jdbc:mysql://localhost:3306/nobugssnackbar", 
+				"com.mysql.jdbc.Driver", "root", "root");
+		
+		r = new Random();
+		s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+		NoBugsConnection nobugs = NoBugsConnection.getConnection();
+		for (int i = 1; i <= 15; i++) {
+			
+			
+			String s1 = extractChar() + extractChar() + extractChar() + extractChar() + extractChar();
+			
+			nobugs.insertUser(s1, UserControl.encrypt(s1), s1, "M");
+		}
+		
+		
+
+	}
+
+}
