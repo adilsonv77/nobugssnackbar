885023068e627e71281abdc3c8db706afdd87069
maciel.hog@gmail.com <maciel.hog@gmail.com@1eb9bf7e-ced2-3440-c91b-7b24e603ad30>
2014-11-18 17:59:45 +0000

    Issue 56 - Continued.

--- trunk/src/pt/uc/dei/nobugssnackbar/util/Image.java
+++ trunk/src/pt/uc/dei/nobugssnackbar/util/Image.java
@@ -1,10 +1,5 @@
 package pt.uc.dei.nobugssnackbar.util;
 
-import java.awt.image.BufferedImage;
-import java.awt.image.DataBufferByte;
-import java.awt.image.WritableRaster;
-import java.io.ByteArrayInputStream;
-import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -12,12 +7,8 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.math.BigInteger;
 
-import javax.imageio.ImageIO;
-import javax.xml.bind.DatatypeConverter;
-
-import org.eclipse.jdt.internal.compiler.parser.Scanner;
+import javax.servlet.ServletContext;
 
 public class Image {
 	
@@ -34,32 +25,22 @@ public class Image {
 		return result.toString();
 	}
 	
-	public static File toImage(String hex, String filename, String filetype) throws IOException {
+	public static byte[] toImage(String hex) throws IOException {
 		int len = hex.length();
 		byte[] data = new byte[len / 2];
 		for (int i = 0; i < len; i += 2) {
 			data[i / 2] = (byte) ((Character.digit(hex.charAt(i), 16) << 4) + Character.digit(hex.charAt(i+1), 16));
 		}
 		
-		//Here are creating a temporary file, but this can be changed
-		File image = File.createTempFile(filename, "." + filetype);
-		
-		OutputStream out = new FileOutputStream(image);
-		out.write(data);
-		out.close();
-		
-		return image;
+		return data;
 	}
 	
-	//Test function, it's functional :)
-	public static void main(String... args) throws Exception {
-		File file = new File("C:\\DSC00919.JPG");
-		
-		String hex = toHex(file);
-		
-		File file2 = toImage(hex, "image", "jpg");
-		
-		System.out.println("FILE: " + file2.getAbsolutePath());
-		
-	}
+//	public static void main(String...strings) throws Exception {
+//		File file = new File("C:\\tela2.png");
+//		
+//		Image image = new Image("");
+//		
+//		System.out.println(image.toHex(file));
+//		
+//	}
 }
