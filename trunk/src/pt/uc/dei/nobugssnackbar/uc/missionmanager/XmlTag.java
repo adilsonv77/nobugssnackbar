134357134618639ffd5e81132def04405d79423d
beytularedzheb@gmail.com <beytularedzheb@gmail.com@1eb9bf7e-ced2-3440-c91b-7b24e603ad30>
2015-03-02 18:24:27 +0000

    Mission manager (UI) - Done!

---
+++ trunk/src/pt/uc/dei/nobugssnackbar/uc/missionmanager/XmlTag.java
@@ -0,0 +1,26 @@
+package pt.uc.dei.nobugssnackbar.uc.missionmanager;
+
+public class XmlTag {
+	private int preload;
+	private boolean alwaysNew;
+	private String xmlns;
+	
+	public int getPreload() {
+		return preload;
+	}
+	public void setPreload(int preload) {
+		this.preload = preload;
+	}
+	public boolean isAlwaysNew() {
+		return alwaysNew;
+	}
+	public void setAlwaysNew(boolean alwaysNew) {
+		this.alwaysNew = alwaysNew;
+	}
+	public String getXmlns() {
+		return xmlns;
+	}
+	public void setXmlns(String xmlns) {
+		this.xmlns = xmlns;
+	}
+}
