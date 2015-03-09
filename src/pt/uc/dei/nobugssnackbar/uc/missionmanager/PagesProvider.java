6f3c7242b795535d4da111e845e4022e71d57869
adilsonv77@gmail.com <adilsonv77@gmail.com@1eb9bf7e-ced2-3440-c91b-7b24e603ad30>
2015-03-09 16:41:40 +0000

    Update issue 90

---
+++ src/pt/uc/dei/nobugssnackbar/uc/missionmanager/PagesProvider.java
@@ -0,0 +1,11 @@
+package pt.uc.dei.nobugssnackbar.uc.missionmanager;
+
+import java.util.List;
+
+import pt.uc.dei.nobugssnackbar.model.Page;
+
+public interface PagesProvider {
+
+	List<Page> getPages();
+	
+}
