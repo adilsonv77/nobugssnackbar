5c81fc91e34ea064274c856b15445cf94f79800b
adilsonv77@gmail.com <adilsonv77@gmail.com@1eb9bf7e-ced2-3440-c91b-7b24e603ad30>
2015-03-12 11:30:24 +0000

    JdbcDao : Read function

--- trunk/src/pt/uc/dei/nobugssnackbar/dao/jdbc/MissionJdbcDao.java
+++ trunk/src/pt/uc/dei/nobugssnackbar/dao/jdbc/MissionJdbcDao.java
@@ -5,33 +5,6 @@ import pt.uc.dei.nobugssnackbar.model.Mission;
 
 public class MissionJdbcDao extends JdbcDao<Mission> implements MissionDao {
 
-	/*
-	public void insertMission(String name, String xml) throws SQLException {
-
-		Connection bdCon = null;
-		try {
-			bdCon = getConnection();
-			PreparedStatement ps = bdCon
-					.prepareStatement("insert into missions (missionname, missioncontent) values (?, ?)");
-			ps.setString(1, name);
-			ps.setString(2, xml);
-
-			ps.executeUpdate();
-			ps.close();
-		} finally {
-			if (bdCon != null)
-				try {
-					bdCon.close();
-				} catch (SQLException ignore) {
-				}
-		}
-	}
-*/
-	@Override
-	public Mission read(long key) throws Exception {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 
 }
