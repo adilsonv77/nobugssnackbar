9e0e1443a272050621520be1b3f669ee9ff40212
adilsonv77@gmail.com <adilsonv77@gmail.com@1eb9bf7e-ced2-3440-c91b-7b24e603ad30>
2015-03-12 08:34:47 +0000

    Developing a hint category solution

--- trunk/src/pt/uc/dei/nobugssnackbar/model/HintCategory.java
+++ trunk/src/pt/uc/dei/nobugssnackbar/model/HintCategory.java
@@ -2,4 +2,50 @@ package pt.uc.dei.nobugssnackbar.model;
 
 public class HintCategory {
 
+	private int id;
+	private String title;
+	private String description;
+	private String body;
+
+	public HintCategory(int id, String title, String description, String body) {
+		this.id = id;
+		this.title = title;
+		this.description = description;
+		this.body = body;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getBody() {
+		return body;
+	}
+
+	public void setBody(String body) {
+		this.body = body;
+	}
+
+	
+	
 }
