fcbe14d01b8be1522f73da732aca7f8983ee072a
ibrahimabtula <ibrahimabtula@gmail.com>
2015-06-15 16:05:20 +0100

    MissionSetting Merging

--- src/pt/uc/dei/nobugssnackbar/uc/missionmanager/MissionSettingsVC.java
+++ src/pt/uc/dei/nobugssnackbar/uc/missionmanager/MissionSettingsVC.java
@@ -168,13 +168,12 @@ public class MissionSettingsVC implements IMissionProvider, Serializable {
 		return xmltag.isLoadBlocks();
 	}
 	public void setLoadBlocks(boolean loadBlocks){
-<<<<<<< db4b44cfe4e9e4ec86cccf70161c5f4fa1ed8e09
-=======
 		if(loadBlocks == false){
 			xmltag.setXmlns("");
 		}
->>>>>>> 23ac64edad58cb11124de12a611eec0f0764a476
-		xmltag.setLoadBlocks(loadBlocks);
+		else{
+			xmltag.setLoadBlocks(loadBlocks);
+		}
 	}
 	
 	public void handlePrevMissionChange() {
