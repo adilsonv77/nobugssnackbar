e26c4a55363a91cea1d1dd87dd104d3da18da96e
adilsonv77 <adilsonv77@gmail.com>
2015-09-15 17:31:14 +0100

    Enhanced the city animation: cars and bus

--- web/js/nobugs/citymap.js
+++ web/js/nobugs/citymap.js
@@ -1,10 +1,27 @@
 PreloadImgs.put('bus', 'images/bus.png');
+
 PreloadImgs.put('city_map1', 'images/city_map.png');
-PreloadImgs.put('city_map2', 'images/city_map2.png');
+
+PreloadImgs.put('busback', 'images/bus_back.png');
+PreloadImgs.put('middleBuildings', 'images/middleBuildings.png');
+PreloadImgs.put('signs', 'images/signs.png');
+PreloadImgs.put('blinkSchool', 'images/blinkSchool.png');
+PreloadImgs.put('school', 'images/school.png');
+PreloadImgs.put('trees', 'images/trees.png');
+
+PreloadImgs.put('carred', 'images/car_red.png');
+PreloadImgs.put('carredback', 'images/car_red_back.png');
+
+PreloadImgs.put('cargreen', "images/car_green.png");
+PreloadImgs.put('cargreendown', "images/car_green_down.png");
 
 var CityMap = {};
 
 CityMap.init = function(options) {
+	
+	CityMap.flagBusDir = false;
+	CityMap.flagCarRedDir = true;
+	CityMap.flagCarGreenDir = false;
 	
 	CityMap.onclick = options.onclick;
 	
@@ -12,16 +29,95 @@ CityMap.init = function(options) {
 	
 	CityMap.bus = {left: 0, top: 100};
 	CityMap.bus.img = new Sprite({
-		ticksPerFrame: 0,
+		ticksPerFrame: 1,
 		numberOfFrames: 2,
 		horzSeq: true,
-		x: 0,
-		y: 8,
+		x: 403,
+		y: -73,
 		width: 160,
 		height: 59,
 		sourceY: 0,
 		img : PreloadImgs.get('bus')
 	});
+	
+	CityMap.busBack = {left: 0, top: 100};
+	CityMap.busBack.img = new Sprite({
+		ticksPerFrame: 1,
+		numberOfFrames: 2,
+		horzSeq: true,
+		x: 400,
+		y: 212,
+		width: 160,
+		height: 59,
+		sourceY: 0,
+		img : PreloadImgs.get('busback')
+	});
+
+	CityMap.car_red  = {left: 0, top: 100};
+	CityMap.car_red.img = new Sprite({
+			ticksPerFrame: 0,
+			numberOfFrames: 2,
+			horzSeq: true,
+			x: -280,
+			y: -152,
+			width: 160,
+			height: 59,
+			sourceY: 0,
+			img : PreloadImgs.get('carred')
+	});
+	
+	CityMap.car_red_back = {left: 0, top: 100};
+	CityMap.car_red_back.img = new Sprite({
+				ticksPerFrame: 0,
+				numberOfFrames: 2,
+				horzSeq: true,
+				x: -161,
+				y: 280,
+				width: 160,
+				height: 59,
+				sourceY: 0,
+				img : PreloadImgs.get('carredback')
+	});
+	
+	CityMap.car_green = {left: 0, top: 100};
+	CityMap.car_green.img = new Sprite({
+				ticksPerFrame: 0,
+				numberOfFrames: 2,
+				horzSeq: true,
+				x: -280,
+				y: -152,
+				width: 160,
+				height: 59,
+				sourceY: 0,
+				img : PreloadImgs.get('cargreen')
+	});
+	
+	CityMap.car_green_down = {left: 0, top: 100}; 
+	CityMap.car_green_down.img = new Sprite({
+				ticksPerFrame: 0,
+				numberOfFrames: 2,
+				horzSeq: true,
+				x: 703,
+				y: -240,
+				width: 160,
+				height: 59,
+				sourceY: 0,
+				img : PreloadImgs.get('cargreendown')
+	});
+
+	
+	CityMap.map1 = PreloadImgs.get('city_map1');
+	
+	CityMap.middleBuildings = PreloadImgs.get("middleBuildings");
+	
+	CityMap.signs = PreloadImgs.get( "signs");
+	
+	CityMap.school_hoover = PreloadImgs.get( "blinkSchool");
+	
+	CityMap.school = PreloadImgs.get("school");
+	
+	CityMap.trees = PreloadImgs.get("trees");
+
 	
 	CityMap.canvas = document.getElementById('city_map');
 	CityMap.canvasCtx = CityMap.canvas.getContext('2d');
@@ -65,7 +161,7 @@ CityMap.pnpoly = function( nvert, vertx, verty, testx, testy ) {
 
 CityMap.startAnimation = function() {
 	CityMap.animate = true;
-	window.setTimeout(CityMap.animateMap, 100);
+	window.setTimeout(CityMap.animateMap, 75);
 };
 
 CityMap.stopAnimation = function() {
@@ -76,11 +172,88 @@ CityMap.animateMap = function() {
 	if (!CityMap.animate) return;
 	
 	CityMap.canvas.style.cursor = (CityMap.blinkSchool?"pointer":"default");
-	CityMap.canvasCtx.drawImage((CityMap.blinkSchool?PreloadImgs.get('city_map2'):PreloadImgs.get('city_map1')), 0, 0, 400, 300);
+	
+	CityMap.canvasCtx.drawImage((CityMap.map1), 0, 0, 400, 300);
+	
+	CityMap.animateBus();
+	CityMap.animateCarRed();
+	CityMap.animateCarGreen();
+	
+	CityMap.canvasCtx.drawImage((CityMap.signs), 0, 0, 400, 300);
+	CityMap.canvasCtx.drawImage((CityMap.middleBuildings), 0, 0, 400, 300);
+	CityMap.canvasCtx.drawImage((CityMap.trees), 0, 0, 400, 300);
+	
+	CityMap.canvasCtx.drawImage((CityMap.blinkSchool?CityMap.school_hoover:CityMap.school), 0, 0, 400, 300);
 
-	CityMap.bus.img.draw(CityMap.canvasCtx);
-   
-	CityMap.bus.img.update();
-   
-	window.setTimeout(CityMap.animateMap, 100);
+	window.setTimeout(CityMap.animateMap, 75);
 };
+
+CityMap.animateBus = function() {
+   if (!CityMap.flagBusDir) {
+	   
+	  CityMap.bus.img.draw(CityMap.canvasCtx);	
+	  CityMap.bus.img.update();
+	  CityMap.flagBusDir = (CityMap.bus.img.x + CityMap.bus.img.width) > 0 ? false : true;
+	  CityMap.bus.img.x -= 1.00 + Math.cos(60 * Math.PI/180);
+	  CityMap.bus.img.y += Math.sin(60 * Math.PI/180);
+	  CityMap.busBack.img.x = 400;
+	  CityMap.busBack.img.y = 212;	  
+	  
+   } else {
+	   
+	 CityMap.bus.img.x = 403;
+	 CityMap.bus.img.y = -73;
+	 CityMap.busBack.img.draw(CityMap.canvasCtx);	
+	 CityMap.busBack.img.update();  
+	 
+	 CityMap.flagBusDir = (CityMap.busBack.img.x + CityMap.busBack.img.width) <= 0 ? false : true;
+   	 
+   	 CityMap.busBack.img.x -= 1.00 + Math.cos(60 * Math.PI/180);
+   	 CityMap.busBack.img.y -= Math.sin(60 * Math.PI/180);	  
+   	 
+   }
+};
+
+CityMap.animateCarRed = function() {
+   if (!CityMap.flagCarRedDir) {
+	   CityMap.car_red.img.draw(CityMap.canvasCtx);	
+	   CityMap.car_red.img.update();
+	   CityMap.flagCarRedDir = CityMap.car_red.img.x >= 400 ? true : false;
+	   CityMap.car_red.img.x += 1.00 + Math.cos(60 * Math.PI/180);
+	   CityMap.car_red.img.y += Math.sin(60 * Math.PI/180);
+	   CityMap.car_red_back.img.x = -161;
+	   CityMap.car_red_back.img.y = 280;
+   }
+   else {
+	   CityMap.car_red.img.x = -280;
+	   CityMap.car_red.img.y = -152;
+	   CityMap.car_red_back.img.draw(CityMap.canvasCtx);	
+	   CityMap.car_red_back.img.update();
+	   CityMap.flagCarRedDir = CityMap.car_red_back.img.x <= 400 ? true : false;
+	   CityMap.car_red_back.img.x += 1.00 + Math.cos(60 * Math.PI/180);
+	   CityMap.car_red_back.img.y -= Math.sin(60 * Math.PI/180);  
+   }
+};
+
+CityMap.animateCarGreen = function() {
+   if (!CityMap.flagCarGreenDir) {
+      CityMap.car_green.img.draw(CityMap.canvasCtx);	
+      CityMap.car_green.img.update();
+      CityMap.flagCarGreenDir = CityMap.car_green.img.x <= 400 ? false : true;
+	  CityMap.car_green.img.x += 1.00 + Math.cos(60 * Math.PI/180);
+	  CityMap.car_green.img.y += Math.sin(60 * Math.PI/180);
+	  CityMap.car_green_down.img.x = 703;
+	  CityMap.car_green_down.img.y = -240;
+   }
+   else {
+      CityMap.car_green.img.x = -280;
+	  CityMap.car_green.img.y = -152;
+      CityMap.car_green_down.img.draw(CityMap.canvasCtx);	
+      CityMap.car_green_down.img.update();  
+      CityMap.flagCarGreenDir = (CityMap.car_green_down.img.x + CityMap.car_green_down.img.width) < 0 ? false : true;
+	  CityMap.car_green_down.img.x -= 1.00 + Math.cos(60 * Math.PI/180);
+	  CityMap.car_green_down.img.y += Math.sin(60 * Math.PI/180);  
+   }
+};
+
+
