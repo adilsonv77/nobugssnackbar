2c65af172ac691f8edfd92b7f0c7f28a845e04a2
adilsonv77 <adilsonv77@gmail.com>
2015-09-29 19:13:43 +0100

    New missions reviewed

--- utils/nobugssnackbar/GenerateHexFromImg.java
+++ utils/nobugssnackbar/GenerateHexFromImg.java
@@ -9,7 +9,7 @@ import pt.uc.dei.nobugssnackbar.util.HexImage;
 public class GenerateHexFromImg {
 
 	public static void main(String[] args) throws FileNotFoundException, IOException {
-		File file = new File("C:/Users/adilsonv77/Pictures/for_aninhados.png");
+		File file = new File("C:/Users/adilsonv77/Pictures/for_aninhados_var.png");
 		System.out.println(HexImage.toHex(file));
 
 	}
