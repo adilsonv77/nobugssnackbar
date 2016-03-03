72363a9887cdcdbc0787a61ddb14da87bc1a46b1
adilsonv77 <adilsonv77@gmail.com>
2016-03-03 11:21:49 +0000

    Forgot to send a file on the last commit

---
+++ src/pt/uc/dei/nobugssnackbar/i18n/Messages_ptbr.java
@@ -0,0 +1,11 @@
+package pt.uc.dei.nobugssnackbar.i18n;
+
+public class Messages_ptbr extends Messages {
+
+	private static final long serialVersionUID = 1L;
+
+	public Messages_ptbr() {
+		this.fileName = "pt-br";
+	}
+	
+}
