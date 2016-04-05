410dcd56980d647f67b330fc66de81b6b6c203f0
adilsonv77 <adilsonv77@gmail.com>
2016-04-05 15:33:56 +0100

    New mission of level 11

--- utils/nobugssnackbar/UpdateMission.java
+++ utils/nobugssnackbar/UpdateMission.java
@@ -17,7 +17,7 @@ public class UpdateMission {
 		MissionJdbcDao mDao = new MissionJdbcDao();
 
 		int j = 0;
-		for (int i = 59; i <= 127; i++) {
+		for (int i = 59; i <= 132; i++) {
 			StringBuffer xml = new StringBuffer();
 			BufferedReader arq = null;
 			try {
